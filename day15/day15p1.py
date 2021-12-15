5a308dfc3cb97c198d138a7e489b4c53bb380c51
regmibishal1 <regmibishal1@gmail.com>
2021-12-15 02:05:53 -0500

    Seperated day 15 into two files

--- day15/day15p1.py
+++ day15/day15p1.py
@@ -1,17 +1,11 @@
 import heapq
 
 
-def findNewCost(arr, rowVal, colVal):
-    x = len(arr)
-    y = len(arr[0])
-    return ((arr[rowVal % x][colVal % y] + (rowVal // x) + (colVal // y)) - 1) % 9 + 1
-
-
 if __name__ == "__main__":
     with open("input.txt") as f:
         lines = f.readlines()
     data = [[int(point) for point in list(line.strip())] for line in lines]
-    m, n = (len(data) * 5, len(data[0]) * 5)
+    m, n = (len(data), len(data[0]))
     costArr = {}
     visited = set()
 
@@ -39,4 +33,4 @@ if __name__ == "__main__":
             if not (0 <= newRow < m and 0 <= newCol < n):
                 continue
             else:
-                heapq.heappush(tempQueue, (findNewCost(data, newRow, newCol) + cost, newRow, newCol))
+                heapq.heappush(tempQueue, (data[newRow][newCol] + cost, newRow, newCol))
